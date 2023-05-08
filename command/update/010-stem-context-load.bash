7364a3d619d936a0fef47290f681c88c42b5cd71
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-08 16:48:18 -0700

    Get dimensions working

--- command/update/010-stem-context-load.bash
+++ command/update/010-stem-context-load.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 stem_context_load() {
+setup_stem_path_vars $stem || return 1
 load_context $stem || return 1
 if [[ ! -d $stem/.cyto ]]; then
   init_stem $stem || return 1
--- command/update/030-each-batch/before-block.bash
+++ command/update/030-each-batch/before-block.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+before_block() {
+
 handle_step_loop() {
   leaf_function=execute_command_step_plain_folder for_each_batch "${batch_dims[@]}"
+}
+
 }
 
