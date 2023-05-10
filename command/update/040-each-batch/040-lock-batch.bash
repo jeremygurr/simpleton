06aaa790e2f182bfafd7a92bba62b253bb285402
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-10 11:37:39 -0700

    Fix lock problem

--- command/update/040-each-batch/040-lock-batch.bash
+++ command/update/040-each-batch/040-lock-batch.bash
@@ -1,14 +1,17 @@
 lock_batch() {
-current_batch_job_path=
-if [[ "$batch_job_path" ]]; then
-  current_batch_job_path=$batch_job_path/current
-  folder_to_lock=$current_batch_job_path folder_lock || fail
-  if [[ "$batch_log_path" ]]; then
-    if [[ ! -d $batch_log_path ]]; then
-      mkdir -p $batch_log_path || fail
+begin_function_flat
+  current_batch_job_path=
+  if [[ "$batch_job_path" ]]; then
+    current_batch_job_path=$batch_job_path/current
+    folder_to_lock=$current_batch_job_path folder_lock || fail
+    if [[ "$batch_log_path" ]]; then
+      if [[ ! -d $batch_log_path ]]; then
+        mkdir -p $batch_log_path || fail
+      fi
+      change_log_file $batch_log_path || fail
     fi
-    change_log_file $batch_log_path || fail
   fi
-fi
+end_function_flat
+handle_return
 }
 
