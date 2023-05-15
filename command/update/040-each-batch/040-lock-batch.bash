60db6686f341efab1c46c6e902587dafb5b954f8
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-15 13:50:54 -0700

    fix logging bug

--- command/update/040-each-batch/040-lock-batch.bash
+++ command/update/040-each-batch/040-lock-batch.bash
@@ -1,20 +1,24 @@
 lock_batch() {
-begin_function_flat
-  current_batch_job_path=
-  if [[ "$batch_job_path" ]]; then
-    current_batch_job_path=$batch_job_path/current
-    folder_to_lock=$current_batch_job_path folder_lock || fail
+current_batch_job_path=
+if [[ "$batch_job_path" ]]; then
+  current_batch_job_path=$batch_job_path/current
+  folder_to_lock=$current_batch_job_path folder_lock || return 1
+fi
+if [[ "$batch_log_path" ]]; then
+  if [[ ! -d $batch_log_path ]]; then
+    mkdir -p $batch_log_path || return 1
   fi
-  if [[ "$batch_log_path" ]]; then
-    if [[ ! -d $batch_log_path ]]; then
-      mkdir -p $batch_log_path || fail
+  if [[ ! "$batch_job_path" ]]; then
+    local contents=( $batch_log_path/* )
+    if [[ "$contents" != $batch_log_path/'*' ]]; then
+      rm $batch_log_path/* 
     fi
-    change_log_file $batch_log_path/update || fail
   fi
-  if [[ "$batch_status_path" ]]; then
-    touch $batch_status_path/last-started-update || fail
-  fi
-end_function_flat
-handle_return
+  change_log_file $batch_log_path/update || return 1
+fi
+if [[ "$batch_status_path" ]]; then
+  touch $batch_status_path/last-started-update || return 1
+fi
+return 0
 }
 
