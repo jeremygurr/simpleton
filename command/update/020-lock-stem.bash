4fca6565b01159705ab4c9caffce781e64a6f3e7
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-15 11:51:33 -0700

    Fix logging bug

--- command/update/020-lock-stem.bash
+++ command/update/020-lock-stem.bash
@@ -3,12 +3,12 @@ current_stem_job_path=
 if [[ "$stem_job_path" ]]; then
   current_stem_job_path=$stem_job_path/current
   folder_to_lock=$current_stem_job_path folder_lock || fail
-  if [[ "$stem_log_path" ]]; then
-    if [[ ! -d $stem_log_path ]]; then
-      mkdir -p $stem_log_path || fail
-    fi
-    change_log_file $stem_log_path/update || fail
+fi
+if [[ "$stem_log_path" ]]; then
+  if [[ ! -d $stem_log_path ]]; then
+    mkdir -p $stem_log_path || fail
   fi
+  change_log_file $stem_log_path/update || fail
 fi
 }
 
--- command/update/040-each-batch/040-lock-batch.bash
+++ command/update/040-each-batch/040-lock-batch.bash
@@ -4,12 +4,12 @@ begin_function_flat
   if [[ "$batch_job_path" ]]; then
     current_batch_job_path=$batch_job_path/current
     folder_to_lock=$current_batch_job_path folder_lock || fail
-    if [[ "$batch_log_path" ]]; then
-      if [[ ! -d $batch_log_path ]]; then
-        mkdir -p $batch_log_path || fail
-      fi
-      change_log_file $batch_log_path/update || fail
+  fi
+  if [[ "$batch_log_path" ]]; then
+    if [[ ! -d $batch_log_path ]]; then
+      mkdir -p $batch_log_path || fail
     fi
+    change_log_file $batch_log_path/update || fail
   fi
   if [[ "$batch_status_path" ]]; then
     touch $batch_status_path/last-started-update || fail
