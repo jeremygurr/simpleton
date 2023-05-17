ef43d0e30a4a7bd3215341a186ff61e28a9a023b
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-17 15:09:39 -0700

    Fix fork logging

--- command/update/040-each-batch/050-each-upstream/before-block.bash
+++ command/update/040-each-batch/050-each-upstream/before-block.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 before_block() {
-# skip before- and choose-
-upstreams=( $(find1 $dna_path/up -not -name '.*' -not -regex '.*/\([0-9]+-\)?\(before\|choose\)-.*')  )
+if [[ "$stem_up_path" ]]; then
+  upstreams=( $(find1 $stem_up_path -not -name '.*' -not -regex '.*/\([0-9]+-\)?\(before\|choose\)-.*')  )
+else
+  upstreams=
+fi
 }
 
