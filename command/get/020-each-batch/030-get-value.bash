925b0d6407620174f94e18f9b93744bb9b4b5a02
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-18 08:15:21 -0700

    Use multi_batch

--- command/get/020-each-batch/030-get-value.bash
+++ command/get/020-each-batch/030-get-value.bash
@@ -3,7 +3,11 @@ if [[ -e $dna_path/op/get ]]; then
   execute_op get || return 1
 else
   if [[ -e $batch_out_path/value ]]; then
-    out "$short_stem: $(<$batch_out_path/value)"
+    if [[ $multi_batch == t ]]; then
+      out "$short_stem: $(<$batch_out_path/value)"
+    else
+      out "$(<$batch_out_path/value)"
+    fi
   else
     warn "$short_stem: No cached value."
   fi
