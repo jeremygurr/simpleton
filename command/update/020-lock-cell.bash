b12e307c0286b655bc7842e81321ead8efca0f5c
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-23 02:53:40 -0700

    continue batch removal migration

---
+++ command/update/020-lock-cell.bash
@@ -0,0 +1,21 @@
+lock_cell() {
+current_job_path=
+if [[ "$job_path" ]]; then
+  current_job_path=$job_path/current
+  folder_to_lock=$current_job_path folder_lock || return 1
+fi
+if [[ "$log_path" ]]; then
+  if [[ ! -d $log_path ]]; then
+    mkdir -p $log_path || fail
+  fi
+  if [[ ! "$job_path" ]]; then
+    local contents=( $log_path/* )
+    if [[ "$contents" != $log_path/'*' ]]; then
+      rm $log_path/* 
+    fi
+  fi
+  change_log_file $log_path/update || return 1
+fi
+return 0
+}
+
--- command/update/050-unlock-stem.bash
+++
@@ -1,8 +0,0 @@
-unlock_stem() {
-if [[ "$stem_job_path" ]]; then
-  folder_to_unlock=$current_stem_job_path folder_unlock || fail
-fi
-if [[ "$stem_log_path" ]]; then
-  change_log_file - || fail
-fi
-}
