c2b78360e2c050c418b56089de2e3492006cb1d0
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-24 12:51:16 -0700

    .

---
+++ command/update/050-make-subs.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+make_subs() {
+if [[ "$sub_path" ]]; then
+  local sub_full sub
+  for sub_full in $(find1 $sub_path -not -name ".*" -not -name "$wild_sub_path" | sort -g); do
+    sub=${sub_full##*/}
+    create_sub_cell $cell_path/$sub || return 1
+  done
+fi
+return 0
+}
+
