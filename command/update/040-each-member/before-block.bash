a8a947086aca567780ea3e538b153949543f61ac
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-24 07:30:52 -0700

    .

--- command/update/040-each-member/before-block.bash
+++ command/update/040-each-member/before-block.bash
@@ -5,7 +5,7 @@ ref_group=none
 before_block() {
 
 handle_step_loop() {
-if [[ "$leaf_dims" ]]; then
+if [[ "${leaf_dims:-}" ]]; then
   function=execute_command_step_plain_folder for_each_member "${leaf_dims[0]}"
 fi
 }
