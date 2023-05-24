431c1ddb8953f7d11d3071964603a58c45b52865
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-24 09:12:09 -0700

    .

--- command/update/040-each-member/010-update-member.bash
+++ command/update/040-each-member/010-update-member.bash
@@ -4,6 +4,9 @@ vars=member; begin_function
   get_sane_value "$member" || fail
   member_path=$cell_path/.dim/$sane_value
   if [[ ! -d $member_path ]]; then
+    if [[ ! -d $cell_path/.dim ]]; then
+      mkdir $cell_path/.dim || fail
+    fi
     create_sub_cell $member_path || fail
   fi
   execute_command $member_path update || fail
