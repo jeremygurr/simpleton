8f94f15f1cc6a5eef6dd615dff32c5f16b6b792b
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-05-26 04:01:19 -0700

    Get sub creation working

--- command/update/050-create-sub-cells.bash
+++ command/update/050-create-sub-cells.bash
@@ -2,6 +2,7 @@
 
 make_subs() {
 if [[ "$sub_path" ]]; then
+  create_sub_cells $cell_path
   local sub_full sub
   for sub_full in $(find1 $sub_path -not -name ".*" -not -name "$wild_sub_path" | sort -g); do
     sub=${sub_full##*/}
