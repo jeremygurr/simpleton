0022fdbd9edffdf68a7935341236bb8e3ca271af
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-06-12 09:32:23 -0700

    Add props

--- command/update/040-each-upstream/010-update-upstream.bash
+++ command/update/040-each-upstream/010-update-upstream.bash
@@ -1,6 +1,12 @@
 update_upstream() {
 if [[ -f $upstream.prep ]]; then
   source $upstream.prep || return 1
+  local f=${upstream//-/_}_prep
+  if ! type -t $f; then
+    fatal "Because $upstream.prep exists, a function called $f must be defined within that file"
+    return 1
+  fi
+  $f || return 1
 fi
 get_node_needs_update $upstream || return 1
 if [[ $needs_update == t ]]; then
