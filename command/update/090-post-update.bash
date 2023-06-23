d83569a230ceaf1ff5ab0ead4bffdf2385cade5c
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-06-23 03:44:48 -0700

    Fix bug

--- command/update/090-post-update.bash
+++ command/update/090-post-update.bash
@@ -1,46 +1,50 @@
 post_update() {
 local result_string
 
-completion_time=${completion_time:-$EPOCHSECONDS}
-if [[ $update_successful == t ]]; then
-  result_string="successful"
-else
-  result_string="failed"
-fi
+begin_function_flat
 
-if [[ $pretend == f ]]; then
-  if [[ "$status_path" ]]; then
-    if [[ $update_successful == t ]]; then
-      touch -d @$completion_time $status_path/last-good-update-end || return 1
-      cp -a $status_path/last-update-start \
-            $status_path/last-good-update-start || return 1
-      if [[ $something_changed == t ]]; then
-        from_cell=$cell_path \
-          propagate_change_to_downstream || return 1
-      fi
-    else
-      touch -d @$completion_time $status_path/last-bad-update-end || return 1
-      cp -a $status_path/last-update-start \
-            $status_path/last-bad-update-start || return 1
-    fi
-  fi
-
-  if [[ $cell_is_leaf == t || $show_branches == t ]]; then
-    info "Update $result_string."
+  completion_time=${completion_time:-$EPOCHSECONDS}
+  if [[ $update_successful == t ]]; then
+    result_string="successful"
   else
-    debug "Update $result_string."
+    result_string="failed"
   fi
 
-else
-  info "Pretend update $result_string."
-fi
+  if [[ $pretend == f ]]; then
+    if [[ "$status_path" ]]; then
+      if [[ $update_successful == t ]]; then
+        touch -d @$completion_time $status_path/last-good-update-end || fail
+        cp -a $status_path/last-update-start \
+              $status_path/last-good-update-start || fail
+        update_prop_hash || fail
+        if [[ $something_changed == t ]]; then
+          from_cell=$cell_path \
+            propagate_change_to_downstream || fail
+        fi
+      else
+        touch -d @$completion_time $status_path/last-bad-update-end || fail
+        cp -a $status_path/last-update-start \
+              $status_path/last-bad-update-start || fail
+      fi
+    fi
 
-if [[ "$job_path" ]]; then
-  folder_to_unlock=$current_job_path folder_unlock || fail
-fi
-if [[ "$log_path" ]]; then
-  change_log_file - || fail
-fi
+    if [[ $cell_is_leaf == t || $show_branches == t ]]; then
+      info "Update $result_string."
+    else
+      debug "Update $result_string."
+    fi
 
-return 0
+  else
+    info "Pretend update $result_string."
+  fi
+
+  if [[ "$job_path" ]]; then
+    folder_to_unlock=$current_job_path folder_unlock || fail
+  fi
+  if [[ "$log_path" ]]; then
+    change_log_file - || fail
+  fi
+
+end_function_flat
+handle_return
 }
