33421d53ffa771aaa59e2f6526589d9491b4f959
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-07-18 10:35:13 -0700

    omni_nav: Add print command

--- command/update/030-create-downstream-links.bash
+++ command/update/030-create-downstream-links.bash
@@ -21,8 +21,8 @@ if [[ "${downstream_ref_path:-}" ]]; then
   else
     local link_target=$(readlink $down_link) || fail
     if [[ $link_target != $down_cell_path ]]; then
-      fatal "Link conflict: A link already exists but doesn't point to the same place."
-      fatal "  $link_target is expected to point to $down_cell_path"
+      log_fatal "Link conflict: A link already exists but doesn't point to the same place."
+      log_fatal "  $link_target is expected to point to $down_cell_path"
     fi
   fi
 fi
