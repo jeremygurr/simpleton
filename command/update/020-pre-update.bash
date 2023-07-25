017f339dd9bfb23497bdff18591402e11fead734
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-07-25 13:30:01 -0700

    Improve job logging and remove feature management

--- command/update/020-pre-update.bash
+++ command/update/020-pre-update.bash
@@ -4,16 +4,6 @@ something_changed=f
 if [[ "$current_job_path" ]]; then
   folder_to_lock=$current_job_path folder_lock || return 1
 fi
-if [[ "$log_path" ]]; then
-  if [[ ! -d $log_path ]]; then
-    mkdir -p $log_path || return 1
-  fi
-  if [[ ! "$job_path" ]]; then
-    rm -rf $log_path || return 1
-    mkdir $log_path || return 1
-  fi
-  change_log_file $log_path/update || return 1
-fi
 if [[ "$status_path" ]]; then
   touch $status_path/last-update-start || return 1
   # this needs to be at the beginning of the update so other processes
