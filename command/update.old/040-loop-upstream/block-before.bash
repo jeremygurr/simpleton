4fc1aa6af80a345ed1f358a9a42e54559a54a4cc
Jeremy Gurr <jeremy.gurr@gmail.com>
2023-09-16 10:20:33 -0700

    Start update command collapse

---
+++ command/update.old/040-loop-upstream/block-before.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+block_before() {
+  if [[ -d "${dna_up_path:-}" && ! "$update_successful" ]]; then
+    upstreams=( $(find1 $dna_up_path \
+      -not -name '.*' \
+      -not -name '*.prep' \
+      -not -regex '.*/\([0-9]+-\)?\(before\|after\|choose\)-.*' \
+      | sort -g \
+      ) )
+    previous_upstream_changed=f
+  else
+    upstreams=
+  fi
+}
+
