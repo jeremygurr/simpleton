8ed89e98b0eafe766424b06db0c741b1d52a60cb
Jeremy Gurr <jeremy.gurr@gmail.com>
2024-01-19 18:35:57 -0700

    Fix needs_update downstream_ref_path bug

--- command/update.old/040-loop-upstream/block-after.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-block_after() {
-  touch $status_path/deps-up-to-date || return 1
-  touch $status_path/up-to-date || return 1
-  return 0
-}
-
--- command/update.old/050-each-member/block-after.bash
+++
@@ -1,6 +0,0 @@
-block_after() {
-  if (( member_count == 1 )); then
-    downstream_ref_path=
-  fi
-}
-
